806e82ee78cb6b0fcfe27488569bc0ba2ca37e5e
missprogrammerr <babithakur246@gmail.com>
2023-09-05 08:39:43 +0545

    Updated code

--- WebSite2/Register.aspx
+++ WebSite2/Register.aspx
@@ -1,17 +1,50 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Register.aspx.cs" Inherits="Register" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="PNCWebAPP1.Register" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-    <link rel="stylesheet" href="css/StyleSheet2.css" />
+<head id="Head1" runat="server">
+    <title>Register</title>
 </head>
 <body>
+    <h1>Register Form</h1>
     <form id="form1" runat="server">
-    <div>
-    
-    </div>
+        <span id="error_messsage" runat="server"></span>
+        <div>
+             <asp:Label runat="server" Text="Name" ID="lblName"/>
+             <asp:TextBox runat="server" ID="txtName" placeholder="Enter Name" />
+             <span ID="errName" runat="server"></span>
+             <br />
+             <asp:Label runat="server" Text="Email" ID="lblEmail"/>
+             <asp:TextBox runat="server" TextMode="Email" ID="txtEmail" placeholder="Enter Email" />
+             <span ID="errEmail" runat="server"></span>
+             <br />
+            <asp:Label runat="server" Text="Phone" ID="lblPhone"/>
+            <asp:TextBox runat="server" ID="txtPhone" placeholder="Enter Phone" />
+            <span ID="errPhone" runat="server"></span>
+            <br />
+            <asp:Label runat="server" Text="Address" ID="lblAddress"/>
+            <asp:TextBox runat="server" ID="txtAddress" placeholder="Enter Address" />
+            <span ID="errAddress" runat="server"></span>
+            <br />
+            <asp:Label runat="server" Text="Gender" ID="lblGender"/>
+            <asp:RadioButtonList ID="rdoGender" runat="server">
+                <asp:ListItem Text="Male" Value="Male" />
+                <asp:ListItem Text="Female" Value="Female" />
+                <asp:ListItem Text="Others" Value="Others" />
+            </asp:RadioButtonList>
+            <span id="errGender" runat="server"></span>
+            <br />
+            <asp:Label runat="server" Text="Username" ID="lblUsername"/>
+            <asp:TextBox runat="server" ID="txtUsername" placeholder="Enter Username" />
+            <span id="errUsername" runat="server"></span>
+            <br />
+            <asp:Label runat="server" Text="Password" ID="lblPassword"/>
+            <asp:TextBox runat="server" TextMode="Password" ID="txtPassword" placeholder="Enter Password" />
+            <span id="errPassword" runat="server"></span>
+            <br />
+            <asp:Button Text="Register" ID="btnRegister" runat="server" OnClick="btnRegister_Click" />
+        </div>
     </form>
 </body>
 </html>
--- WebSite2/Register.aspx.cs
+++ WebSite2/Register.aspx.cs
@@ -1,14 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
 
-public partial class Register : System.Web.UI.Page
+namespace PNCWebAPP1
 {
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class Register : System.Web.UI.Page
     {
+        private string conn;
+        private MySqlConnection connect;
 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            errGender.InnerText = errName.InnerText = errEmail.InnerText = errPhone.InnerText = errAddress.InnerText = errUsername.InnerText = errPassword.InnerText = String.Empty;
+            //get form data
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string phone = txtPhone.Text;
+            string address = txtAddress.Text;
+            string gender = rdoGender.Text;
+
+            int error = 0;
+            if (String.IsNullOrEmpty(name))
+            {
+                errName.InnerText = "Enter name";
+                error++;
+            }
+            if (String.IsNullOrEmpty(email))
+            {
+                errEmail.InnerText = "Enter email";
+                error++;
+            }
+            if (String.IsNullOrEmpty(phone))
+            {
+                errPhone.InnerText = "Enter phone";
+                error++;
+            }
+            if (String.IsNullOrEmpty(address))
+            {
+                errAddress.InnerText = "Enter address";
+                error++;
+            }
+            if (String.IsNullOrEmpty(gender))
+            {
+                errGender.InnerText = "Enter gender";
+                error++;
+            }
+            if (String.IsNullOrEmpty(username))
+            {
+                errUsername.InnerText = "Enter uername";
+                error++;
+            }
+            if (password == "")
+            {
+                errPassword.InnerText = "Enter password";
+                error++;
+            }
+
+            if (error == 0)
+            {
+                //database connection
+                try
+                {
+                    conn = "Server=localhost;Database=college;Uid=root;Pwd=;";
+                    connect = new MySqlConnection(conn);
+                    connect.Open();
+                    MySqlCommand cmd = new MySqlCommand("insert into students(name,email,phone,address,gender,username,password,status) values (@n,@e,@ph,@a,@g,@u,@p,@s)", connect);
+                    cmd.Parameters.AddWithValue("@n", name);
+                    cmd.Parameters.AddWithValue("@e", email);
+                    cmd.Parameters.AddWithValue("@ph", phone);
+                    cmd.Parameters.AddWithValue("@a", address);
+                    cmd.Parameters.AddWithValue("@g", gender);
+                    cmd.Parameters.AddWithValue("@u", username);
+                    cmd.Parameters.AddWithValue("@p", password);
+                    cmd.Parameters.AddWithValue("@s", true);
+                    cmd.ExecuteNonQuery();
+                    error_messsage.InnerText = "Register Success";
+
+                }
+                catch (MySqlException mex)
+                {
+                    error_messsage.InnerText = "Error at Register Page" + mex.Message;
+                }
+            }
+
+        }
     }
 }
