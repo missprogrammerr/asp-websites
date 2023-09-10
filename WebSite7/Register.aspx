2f0ae14c97fad1601ea29ff102487a1b33f2aa32
missprogrammerr <babithakur246@gmail.com>
2023-09-10 08:49:15 +0545

    updated code

---
+++ WebSite7/Register.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Register.aspx.cs" Inherits="Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <span id="error_messsage" runat="server"></span>
+        <div>
+             <asp:Label runat="server" Text="Name" ID="lblName"/>
+             <asp:TextBox runat="server" ID="txtName" placeholder="Enter Name" />
+             <span ID="errName" runat="server"></span>
+             <br /><br />
+             <asp:Label runat="server" Text="Email" ID="lblEmail"/>
+             <asp:TextBox runat="server" TextMode="Email" ID="txtEmail" placeholder="Enter Email" />
+             <span ID="errEmail" runat="server"></span>
+             <br /><br />
+            <asp:Label runat="server" Text="Phone" ID="lblPhone"/>
+            <asp:TextBox runat="server" ID="txtPhone" placeholder="Enter Phone" />
+            <span ID="errPhone" runat="server"></span>
+            <br /><br />
+            <asp:Label runat="server" Text="Gender" ID="lblGender"/>
+            <asp:RadioButtonList ID="rdoGender" runat="server">
+                <asp:ListItem Text="Male" Value="Male" />
+                <asp:ListItem Text="Female" Value="Female" />
+                <asp:ListItem Text="Others" Value="Others" />
+            </asp:RadioButtonList>
+            <span id="errGender" runat="server"></span>
+            <br /><br />
+            <asp:Label runat="server" Text="Faculty" ID="lblFaculty" />
+            <asp:DropDownList ID="drpFaculty" runat="server">
+                <asp:ListItem Text="Humanities & Social Science" Value="humanities_and_social" />
+                <asp:ListItem Text="Management" Value="management" />
+                <asp:ListItem Text="Law" Value="law" />
+                <asp:ListItem Text="Education" Value="education" />
+            </asp:DropDownList>
+            <span id="errFaculty" runat="server"></span>
+            <br /><br />
+            <asp:Label runat="server" Text="Password" ID="lblPassword" />
+            <asp:TextBox runat="server" TextMode="Password" ID="txtPassword1" />
+            <span id="errPassword1" runat="server"></span>
+            <br /><br />
+            <asp:Label runat="server" Text="Confirm-Password" ID="lblPassword2" />
+            <asp:TextBox runat="server" TextMode="Password" ID="txtPassword2" />
+            <span id="errPassword2" runat="server"></span>
+            <br /><br />
+            <asp:Button runat="server" Text="Register" ID="btnRegister" OnClick="btnRegister_Click"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebSite7/Register.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnRegister_Click(object sender, EventArgs e)
+    {
+        errGender.InnerText = errName.InnerText = errEmail.InnerText = errPhone.InnerText = errFaculty.InnerText = errPassword1.InnerText = errPassword2.InnerText = String.Empty;
+            //get form data
+            string password1 = txtPassword1.Text;
+            string password2 = txtPassword2.Text;
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string phone = txtPhone.Text;
+            string gender = rdoGender.Text;
+            string faculty = drpFaculty.Text;
+            
+
+            int error = 0;
+            if (String.IsNullOrEmpty(name))
+            {
+                errName.InnerText = "Enter name";
+                error++;
+            }
+            if (String.IsNullOrEmpty(email))
+            {
+                errEmail.InnerText = "Enter email";
+                error++;
+            }
+            if (String.IsNullOrEmpty(phone))
+            {
+                errPhone.InnerText = "Enter phone";
+                error++;
+            }
+            if (String.IsNullOrEmpty(gender))
+            {
+                errGender.InnerText = "Choose gender";
+                error++;
+            }
+            if (String.IsNullOrEmpty(faculty))
+            {
+                errFaculty.InnerText = "Choose faculty";
+                error++;
+            }
+            if (String.IsNullOrEmpty(password1))
+            {
+                errPassword1.InnerText = "Enter password";
+                error++;
+            }
+            if (String.IsNullOrEmpty(password2))
+            {
+                errPassword1.InnerText = "Re-type password";
+                error++;
+            }
+
+            if (error == 0)
+            {
+                //database connection
+                try
+                {
+                    conn = "Server=localhost;Database=college;Uid=root;Pwd=;";
+                    connect = new MySqlConnection(conn);
+                    connect.Open();
+                    MySqlCommand cmd = new MySqlCommand("insert into students(name,email,phone,faculty,gender,password) values (@n,@e,@ph,@f,@g,@p)", connect);
+                    cmd.Parameters.AddWithValue("@n", name);
+                    cmd.Parameters.AddWithValue("@e", email);
+                    cmd.Parameters.AddWithValue("@ph", phone);
+                    cmd.Parameters.AddWithValue("@f", faculty);
+                    cmd.Parameters.AddWithValue("@g", gender);
+                    cmd.Parameters.AddWithValue("@p", password1);
+                    cmd.ExecuteNonQuery();
+                    error_messsage.InnerText = "Register Success";
+
+                }
+                catch (MySqlException mex)
+                {
+                    error_messsage.InnerText = "Error at Register Page" + mex.Message;
+                }
+            }
+
+        }
+}
